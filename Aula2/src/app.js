5cd9fcf6196c9990f5a48a6db5bf16b831877396
gustavo <gusgurtavo@gmail.com>
2019-02-16 16:47:48 -0200

    fazendo o CRUD

--- Aula2/src/app.js
+++ Aula2/src/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next)=>{
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title:"node Store API",
         autor:"Gustavo Garcia Pereira",
@@ -13,6 +13,28 @@ const route = router.get('/', (req, res, next)=>{
         versio:"0.0.1"
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    res.status(201).send({
+        id:id, 
+        item: req.body
+    });
+});
+
+const del = router.delite('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+
 app.use('/',route);
+app.use('/products',create);
+app.use('/products',put);
+app.use('/products',del);
 
 module.exports = app;
